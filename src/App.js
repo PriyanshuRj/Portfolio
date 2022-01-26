fef80ee78aee11e7c5a6269ac2dc72d182246495
PriyanshuRj <Priyanshurajput@0071109@gmail.com>
2022-01-26 20:47:55 +0530

    Removed exess code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
 import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
 import { themes } from "./theme";
