1597aa8f82d16a9c552460623d319122e67f6c9c
PriyanshuRj <priyanshurajput0071109@gmail.com>
2022-02-01 01:46:57 +0530

    Updates

---
+++ precache-manifest.e5a58fbf6ac0b16bbc3b556d4683ca27.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ca077bdb1768fbb19e60399f89c876",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "3d2026f3d8464b38702d",
+    "url": "/Portfolio/static/css/main.5b70d407.chunk.css"
+  },
+  {
+    "revision": "8888ac6ee1ab01828c03",
+    "url": "/Portfolio/static/js/2.28882869.chunk.js"
+  },
+  {
+    "revision": "35efc362ebec8685794e3c74e7c764c6",
+    "url": "/Portfolio/static/js/2.28882869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d2026f3d8464b38702d",
+    "url": "/Portfolio/static/js/main.1a9515aa.chunk.js"
+  },
+  {
+    "revision": "0ec7fe1802a225e5ee7d",
+    "url": "/Portfolio/static/js/runtime-main.bdd09ce5.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/Portfolio/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/Portfolio/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/Portfolio/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/Portfolio/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/Portfolio/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/Portfolio/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "799d9bd6cc3968b5e70af9a343cd3d2c",
+    "url": "/Portfolio/static/media/appdeployer.799d9bd6.png"
+  },
+  {
+    "revision": "e5cee7b14f892878b06e3aec5e5e08ee",
+    "url": "/Portfolio/static/media/courcera.e5cee7b1.png"
+  },
+  {
+    "revision": "add0381ab446895f962f1e55df90830e",
+    "url": "/Portfolio/static/media/dl.add0381a.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/Portfolio/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/Portfolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "/Portfolio/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/Portfolio/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/Portfolio/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/Portfolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "/Portfolio/static/media/fa-regular-400.9443c1e7.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/Portfolio/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/Portfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/Portfolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/Portfolio/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/Portfolio/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "/Portfolio/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/Portfolio/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/Portfolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "afa7c52b1ae3946bff066429766242d0",
+    "url": "/Portfolio/static/media/gssoc.afa7c52b.png"
+  },
+  {
+    "revision": "ae0e9516fd132ccf29b498295ac53238",
+    "url": "/Portfolio/static/media/ml.ae0e9516.png"
+  },
+  {
+    "revision": "d324ee17433678f2c32e57198b98c6db",
+    "url": "/Portfolio/static/media/mongo.d324ee17.png"
+  },
+  {
+    "revision": "9cfef252f69c7339390920f387460eac",
+    "url": "/Portfolio/static/media/nith.9cfef252.png"
+  },
+  {
+    "revision": "e267c63e665d81f22bb2579f79099efe",
+    "url": "/Portfolio/static/media/prepbytes.e267c63e.png"
+  },
+  {
+    "revision": "ed3a6552bacfeef8b70810b9ae49eb4f",
+    "url": "/Portfolio/static/media/self.ed3a6552.jpg"
+  },
+  {
+    "revision": "376d4835901f9041567d6c2001775c5d",
+    "url": "/Portfolio/static/media/self.jpg.376d4835.png"
+  },
+  {
+    "revision": "d843cc327894bc85dec2294a30d1dcee",
+    "url": "/Portfolio/static/media/vbps.d843cc32.png"
+  },
+  {
+    "revision": "4b939317cc075a85f55b140943f1b4b5",
+    "url": "/Portfolio/static/media/web3.4b939317.png"
+  },
+  {
+    "revision": "7b59eb51ca17e80affbbe9d2aa4db05c",
+    "url": "/Portfolio/static/media/webd.7b59eb51.png"
+  },
+  {
+    "revision": "06a4679ecbdd31efc7d4f9b9e93d5b6e",
+    "url": "/Portfolio/static/media/webd2.06a4679e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Resume/precache-manifest.57abf131812e1942858446c04d48eb41.js"
+  "/Portfolio/precache-manifest.e5a58fbf6ac0b16bbc3b556d4683ca27.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Resume/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
