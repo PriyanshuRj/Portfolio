e3f4434b59b07cecea57369ede8833dfe50c2efd
PriyanshuRj <priyanshurajput0071109@gmail.com>
2022-03-05 22:33:27 +0530

    Updated profile

--- src/portfolio.js
+++ src/portfolio.js
@@ -654,7 +654,7 @@ const projectsHeader = {
 const contactPageData = {
   contactSection: {
     title: 'Contact Me',
-    profile_image_path: 'self.png',
+    profile_image_path: 'self.jpeg',
 
     description:
       'You can contact me at the places mentioned below. I will try to get back to you as fast as I can. '
